f9024a0f8e9285b450e4bc3412001d64d026ce90
lipe753 <112371646+lipe753@users.noreply.github.com>
2022-10-21 12:13:32 +0000

    Beginning

---
+++ .ccls-cache/@home@runner@Revisando/combinacao.cpp
@@ -0,0 +1,3 @@
+#include "combinacao.hpp"
+
+combinacao::combinacao(char _letra, std::string _numero): letra(_letra), numero(_numero){}
---
+++ .ccls-cache/@home@runner@Revisando/combinacao.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+class combinacao{
+
+private:
+
+char letra;
+std::string numero;
+
+public:
+
+combinacao(char _letra, std::string _numero);
+
+
+}
