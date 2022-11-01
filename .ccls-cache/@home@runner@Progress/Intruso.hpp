a5b946f6a9615b90d03865f182b5ae40ed05ecc7
Lucas  Araújo <112007865+Lucas-resende21@users.noreply.github.com>
2022-11-01 12:31:49 +0000

    ignore antes do for

---
+++ .ccls-cache/@home@runner@Progress/Intruso.hpp
@@ -0,0 +1,21 @@
+#ifndef INTRUSO_HPP
+#define INTRUSO_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+class Intruso{
+
+    private:
+      std::map<char,std::string> digitos;
+	  std::vector<std::map<char, std::string> > tudo;
+	  std::vector<std::string> senhas;
+    public:
+      Intruso()=default;
+      void set_senha_vazada(std::string vazou);
+      std::string crack_senha();
+};
+
+#endif
--- Makefile
+++ Makefile
@@ -1,15 +1,5 @@
-all: main
-
-CXX = clang++
-override CXXFLAGS += -g -Wno-everything
-
-SRCS = $(shell find . -name '.ccls-cache' -type d -prune -o -type f -name '*.cpp' -print | sed -e 's/ /\\ /g')
-
-main: $(SRCS)
-	$(CXX) $(CXXFLAGS) $(SRCS) -o "$@"
-
-main-debug: $(SRCS)
-	$(CXX) $(CXXFLAGS) -O0 $(SRCS) -o "$@"
-
+all:
+	g++ *.cpp -o main.out
+    
 clean:
-	rm -f main main-debug
+	$(RM) -r main.out
